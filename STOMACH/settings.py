a556e0d45a03ffb251eb9e3acc6dc9c8c676d16d
Juergen Durner <durnerj@googlemail.com>
2017-05-16 20:46:54 +0200

    renamed appname in class

--- STOMACH/settings.py
+++ STOMACH/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stomach_src.apps.RenameMeConfig',
+    'stomach_src.apps.stomach_srcConfig',
 ]
 
 MIDDLEWARE = [
--- stomach_src/apps.py
+++ stomach_src/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RenameMeConfig(AppConfig):
+class stomach_srcConfig(AppConfig):
     name = 'stomach_src'
