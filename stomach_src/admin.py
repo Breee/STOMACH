6e2701c4afe7e4a2c9f7bac68462335f48a14847
Breee <loeffler.uni@gmail.com>
2017-05-17 14:14:06 +0200

    testing

--- stomach_src/admin.py
+++ stomach_src/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe,Ingredient,Ing_Recipe
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ing_Recipe
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,               {'fields': ['name', 'description', 'category','cook_time','person_amount']}),
+            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+                ]
+    inlines = [IngredientInline]
+
+admin.site.register(Recipe,RecipeAdmin)
+admin.site.register(Ingredient)
+admin.site.register(Ing_Recipe)
 
 # Register your models here.
--- stomach_src/models.py
+++ stomach_src/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    BF = "breakfast"
+    BR = "brunch"
+    LU = "lunch"
+    CATEGORY_CHOICES = (
+        (BF, "breakfast"),
+        (BR, "brunch"),
+        (LU, "lunch"))
+
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    category = models.CharField(max_length=200,choices=CATEGORY_CHOICES, default=BF)
+    description = models.CharField(max_length=1000)
+    cook_time = models.PositiveIntegerField(default=0)
+    person_amount = models.PositiveIntegerField(default=1)
+    #tag_recipe_ID
+
+    def __str__(self):
+        return self.name + " (ID: %d)" % self.id
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Ing_Recipe(models.Model):
+    G = "grams(g)"
+    KG = "kilograms(kg)"
+    L = "litre(l)"
+    UNIT_CHOICES = (
+        (G, "grams(g)"),
+        (KG, "kilograms(kg)"),
+        (L, "litre(l)"))
+    recipe_ID = models.ForeignKey(Recipe)
+    ing_ID = models.ForeignKey(Ingredient)
+    value = models.PositiveIntegerField(default=1)
+    unit = models.CharField(max_length=50, choices=UNIT_CHOICES)
+
