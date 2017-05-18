1f2593049b02f3c99212cff471413d92edb09e21
Unknown <loeffler.uni@gmail.com>
2017-05-18 20:05:24 +0200

    comments

--- stomach_src/views.py
+++ stomach_src/views.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 from .models import Recipe
 
 def index(request):
+    # simply gets the 5 most recent created recipes and orders them by publishing date
     latest_recipes_list = Recipe.objects.order_by('-pub_date')[:5]
+
+    # context is a map with elements of the form (id -> iterable)
+    # where the id is the name accessible in html templates.
     context = {'latest_recipes_list': latest_recipes_list}
+
+    # render will replace the python code in the template with whatever you defined.
     return render(request, 'html/index.html', context)
