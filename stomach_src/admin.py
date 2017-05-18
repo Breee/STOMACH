1e4f55be68ebca7526505aef719e241ecf498833
Unknown <loeffler.uni@gmail.com>
2017-05-18 17:58:42 +0200

    models /admin

--- stomach_src/admin.py
+++ stomach_src/admin.py
@@ -1,20 +1,38 @@
 from django.contrib import admin
-from .models import Recipe,Ingredient,Ing_Recipe
+from .models import Recipe
+from .models import Ingredient
+from .models import Ing_Recipe
+from .models import Category
+from .models import Category_Recipe
+from .models import Tag
+from .models import Tag_Recipe
+from .models import Unit
+
+
 
 
 class IngredientInline(admin.TabularInline):
     model = Ing_Recipe
-    extra = 1
+
+class CategoryInline(admin.TabularInline):
+    model = Category_Recipe
+
+class TagInline(admin.TabularInline):
+    model = Tag_Recipe
+
 
 class RecipeAdmin(admin.ModelAdmin):
     fieldsets = [
-            (None,               {'fields': ['name', 'description', 'category','cook_time','person_amount']}),
+            (None,               {'fields': ['name', 'description', 'cook_time','person_amount']}),
             ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
                 ]
-    inlines = [IngredientInline]
+    inlines = [IngredientInline,TagInline,CategoryInline]
 
 admin.site.register(Recipe,RecipeAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Ing_Recipe)
-
-# Register your models here.
+admin.site.register(Category_Recipe)
+admin.site.register(Tag_Recipe)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Unit)
--- stomach_src/models.py
+++ stomach_src/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # class for units like gram,litre,cups,ounces
 class Unit(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
@@ -17,6 +18,7 @@ class Category(models.Model):
 # class for tags which shall be attached to recipes, e.g. flour, oven..
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
@@ -25,12 +27,16 @@ class Recipe(models.Model):
 
     # name of the recipe
     name = models.CharField(max_length=200)
+
     # date on which the recipe is/shall be published.
     pub_date = models.DateTimeField('date published')
+
     # description of the recipe (how to cook xxx..)
     description = models.CharField(max_length=1000)
+
     # time to prepare/cook the recipe.
     cook_time = models.PositiveIntegerField(default=0)
+
     # amount of persons.
     person_amount = models.PositiveIntegerField(default=1)
 
@@ -45,11 +51,18 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
-# class that defines a mapping [ingedient] -> [recipe]
+# class that defines a mapping [ingredient] -> [recipe]
 class Ing_Recipe(models.Model):
+    # recipe id
     recipe_ID = models.ForeignKey(Recipe)
+
+    # ingredient id
     ing_ID = models.ForeignKey(Ingredient)
-    value = models.PositiveIntegerField(default=1)
+
+    # amount of the ingredient
+    amount = models.PositiveIntegerField(default=1)
+
+    # unit of the ingredient
     unit = models.ForeignKey(Unit)
 
 # class that defines a mapping [category] -> [recipe]
