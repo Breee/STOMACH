78cf181e24565e74087c6cbf2ebc70293fb08977
Breee <loeffler.uni@gmail.com>
2017-06-16 18:11:12 +0200

    search delay, search only if query has changed.

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,6 +1,11 @@
 /**
  * Created by Bree on 20.05.2017.
  */
+
+// Whole-script strict mode syntax
+'use strict';
+var lastQuery = "";
+
 $(document).ready(function () {
     // prevents that arrays in get requests get encoded with brackets.
     jQuery.ajaxSettings.traditional = true;
@@ -18,13 +23,15 @@ $(document).ready(function () {
     });
 
     $('#searchbar').keyup(function (evt) {
-        getRecipesAndFilters();
+        delay(function () {
+            getRecipesAndFilters();
+        }, 200);
     });
 
-    $(document).on('click','.recipefilter',function (evt) {
-        if($(this).hasClass("selected-filter")){
+    $(document).on('click', '.recipefilter', function (evt) {
+        if ($(this).hasClass("selected-filter")) {
             $(this).removeClass("selected-filter");
-        }else{
+        } else {
             $(this).addClass("selected-filter");
         }
         getRecipesAndFilters();
@@ -32,13 +39,24 @@ $(document).ready(function () {
 
 });
 
+var delay = (function () {
+    var timer = 0;
+    return function (callback, ms) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
+
 function getRecipesAndFilters() {
+    var query = $('#searchbar').val();
+    if(lastQuery.trim() != query.trim()){
+        lastQuery = query;
     $.get('/recipes/', {'q': $('#searchbar').val(), 'initialized': 1, 'filter': getSelectedFilters()}, function (data) {
-            var query = $('#searchbar').val();
-            console.log(data);
-            prepareRecipeList(data.latest_recipes_list);
-            prepareFilters(data.filters, data.selected_filters, data.filter_string, query);
-        });
+        var query = $('#searchbar').val();
+        prepareRecipeList(data.latest_recipes_list);
+        prepareFilters(data.filters, data.selected_filters);
+    });
+    }
 }
 
 function getSelectedFilters() {
@@ -54,36 +72,40 @@ function prepareRecipeList(recipe_list) {
         var elem = '<li><a href="/recipes/' + el.rec_id + '">' + el.name + '(Published: ' + el.published_date + ')</a></li>';
         recipe_html += elem;
     });
-    $("#recipe-list").html(recipe_html);
+    if (recipe_html) {
+        $("#recipe-list").html(recipe_html);
+    } else {
+        $("#recipe-list").html("<p> No results available </p>");
+    }
 }
 
-function prepareFilters(filters, selected_filters, filter_string, query) {
-    filter_string = filter_string != '""' ? filter_string : '';
+function prepareFilters(filters, selected_filters) {
+
     var filters_html = "";
 
-   $.each(JSON.parse(selected_filters), function (i, el) {
+    $.each(JSON.parse(selected_filters), function (i, el) {
         // e.g. <a class="list-group-item recipefilter selected-filter" id="54">Vegetarian<span class="badge">5</span></a>
         var elem = ("<a class=\"list-group-item recipefilter selected-filter\" id=\"")
-             .concat(el.id)
-             .concat("\">")
-             .concat(el.name)
-             .concat("</a>");
+            .concat(el.id)
+            .concat("\">")
+            .concat(el.name)
+            .concat("</a>");
         filters_html += elem;
     });
 
     $.each(JSON.parse(filters), function (i, el) {
         // e.g. <a class="list-group-item recipefilter" id="54">Vegetarian<span class="badge">5</span></a>
         var elem = ("<a class=\"list-group-item recipefilter\" id=\"")
-             .concat(el.category_ID)
-             .concat("\">")
-             .concat(el.category_ID__name)
-             .concat("<span class=\"badge\">")
-             .concat(el.count)
-             .concat("</span>")
-             .concat("</a>");
+            .concat(el.category_ID)
+            .concat("\">")
+            .concat(el.category_ID__name)
+            .concat("<span class=\"badge\">")
+            .concat(el.count)
+            .concat("</span>")
+            .concat("</a>");
         filters_html += elem;
     });
-    console.log(filters_html);
+
     $(".filters-content").empty().append(filters_html);
 }
 function validateIngredientForm() {
